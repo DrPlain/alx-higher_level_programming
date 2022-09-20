b381306a7991d808231ee2a71798f944350b910e
Gideon Obiasor <gideonobiasor@gmail.com>
2022-09-20 19:42:32 +0000

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for number in range(1, 101):
+        if (number % 3 == 0) | (number % 5 == 0):
+            if (number % 3 == 0) & (number % 5 != 0):
+                print("Fizz", end=" ")
+            if (number % 5 == 0) & (number % 3 != 0):
+                print("Buzz", end=" ")
+            if (number % 3 == 0) & (number % 5 == 0):
+                print("FizzBuzz", end=" ")
+        else:
+            print(number, end=" ")
