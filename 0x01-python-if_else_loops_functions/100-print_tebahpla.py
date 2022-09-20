5aba686829875eeec895e3ba11c1cad2515d6f4e
Gideon Obiasor <gideonobiasor@gmail.com>
2022-09-20 20:57:50 +0000

    Prints ASCII alphabet in reverse order, alternating lowercase and uppercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,6 +3,5 @@
 asci_upper = 91
 
 for number in range(122, 96, -2):
-    print("{:c}".format(number), end="")
     asci_upper -= 2
-    print("{:c}".format(asci_upper), end="")
+    print("{:c}{:c}".format(number, asci_upper), end="")
