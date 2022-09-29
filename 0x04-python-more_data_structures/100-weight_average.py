e311ef3f567d4fea23fdbc89ba3486813b5240eb
Gideon Obiasor <gideonobiasor@gmail.com>
2022-09-29 00:04:22 +0000

    Tasks on lambda, map, filter, set and dict

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if not isinstance(my_list, list) or len(my_list) == 0:
-        return None
+        return 0
     score_t = 0
     wt_t = 0
     for tupl in my_list:
