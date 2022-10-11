80d302e652db99d05af9edadcfc829d394b7b340
= <=>
2022-10-11 16:28:02 +0100

    Task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,11 +12,8 @@ class Square:
             size (int): These size of the square.
             position (tuple): Represents coordinates of a square
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
+        self.__position = position
 
     @property
     def size(self):
@@ -40,6 +37,23 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        """A getter to retrive square position.
+
+        Return:
+            position (tuple): coordinates of the square.
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets the new coordinates of the square"""
+
+        if not isinstance(value, tuple) or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def area(self):
         """Returns current square area.
 
@@ -52,8 +66,13 @@ class Square:
         """Prints the square with '#' in stdout."""
         if self.__size == 0:
             print()
-        else:
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#", end='')
-                print()
+            return
+        if self.__position[1] > 0:
+            print()
+
+        for i in range(self.__size):
+            for index in range(self.__position[0]):
+                print(" ", end='')
+            for j in range(self.__size):
+                print("#", end='')
+            print()
