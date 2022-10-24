7195e3ee93237e86478e14b245ac4dbf339f1b9d
= <=>
2022-10-24 16:06:06 +0100

    Task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-Defines the class MyList
+contains the MyList class
 """
 
 
 class MyList(list):
-    """A subclass that inherits from list as superclass"""
+    """a subclass of list"""
     def __init__(self):
         """initializes the object"""
         super().__init__()
 
     def print_sorted(self):
-        """Prints the list sorted in ascending order"""
+        """prints the sorted list"""
         print(sorted(self))
