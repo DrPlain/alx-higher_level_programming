41e12f915a3be6e0e2ec4ff4141598dd6a49c9ae
= <=>
2022-10-30 19:44:19 +0000

    Task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,8 +19,20 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list-dictionaries"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON str representation of list_objs to a file"""
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w') as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                dict_list = [obj.to_dictionary() for obj in list_objs]
+                jsonfile.write(Base.to_json_string(dict_list))
