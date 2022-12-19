b9bea8e95a22cba3c7074a91568b05b459d78922
Gideon Obiasor <gideonoiasor@gmail.com>
2022-12-19 14:50:13 +0100

    Task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 
-console.log('C is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
+const c = 'C is fun';
+const python = 'Python is cool';
+const javascript = 'Javascript is amazing';
+
+console.log(`${c}\n${python}\n${javascript}`);
