1ca096bf29fdd4eaf33c7bb1bce258488e0b4a05
Gideon Obiasor <gideonoiasor@gmail.com>
2022-12-21 14:23:06 +0100

    Task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""lists all states with a name starting with N(upper N) from db hbtn_0e_0_usa
+    Usage: ./0-select_states.py <mysql username> \
+                                <mysql passwd> \
+                                <database name> \
+                                <state name searched>
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s \
+            ORDER BY states.id", (argv[4],))
+    states = cursor.fetchall()
+    [print(state) for state in states if state[1] == argv[4]]
+    cursor.close()
+    db.close()
