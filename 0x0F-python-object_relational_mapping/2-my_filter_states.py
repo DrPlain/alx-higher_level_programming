a5acd8dc20347d6ceb2cecb9172cc6014c1b7d78
Gideon Obiasor <gideonoiasor@gmail.com>
2022-12-21 12:30:04 +0100

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """lists all states with a name starting with N(upper N) from db hbtn_0e_0_usa
-    Usage: ./0-select_states.py <mysql username> <mysql passwd> <database name>
+    Usage: ./0-select_states.py <mysql username> \
+                                <mysql passwd> \
+                                <database name> \
+                                <state name searched>
 """
 import MySQLdb
 import sys
