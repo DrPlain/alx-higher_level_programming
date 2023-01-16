47582ce6aa604471c4ec1ff8e4d2dc05a9a5f102
Gideon Obiasor <gideonoiasor@gmail.com>
2023-01-16 16:53:51 +0100

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # Takes an url, sends a request & displays the content-length
-curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f2
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
