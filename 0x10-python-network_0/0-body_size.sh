7dac4e8a0d9a1a72b1c062b10eaa053e6f54be69
Gideon Obiasor <gideonoiasor@gmail.com>
2023-01-16 16:20:06 +0100

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/bash
-# A script that takes in a url, sends a request to that URL
-# and displays the size of the body of the response
-# -s == silent mode, -I == display only head
-
+# Takes an url, sends a request & displays the content-length
 curl -sI "$1" | grep -i Content-Length | cut -d ':' -f2
