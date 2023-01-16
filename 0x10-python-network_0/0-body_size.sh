218a3220f9cd806b528e48b79ab80b7525340f0b
Gideon Obiasor <gideonoiasor@gmail.com>
2023-01-16 16:18:02 +0100

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -3,4 +3,4 @@
 # and displays the size of the body of the response
 # -s == silent mode, -I == display only head
 
-curl -s -I "$1" | grep "content-length" | cut -d ':' -f2
+curl -sI "$1" | grep -i Content-Length | cut -d ':' -f2
