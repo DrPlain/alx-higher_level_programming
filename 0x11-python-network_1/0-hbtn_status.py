aec40e3ca5af14c0a65b747d6b5dd2f3456300e4
Gideon Obiasor <gideonoiasor@gmail.com>
2023-01-17 13:22:06 +0100

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         content = response.read()
-        print("Body response:\n     - type: {}\n     - content: {}\n\
-     - utf8 content: OK".format(
-            type(content), content))
+        print("Body response:")
+        print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(
+            type(content), content,
+            content.decode('utf-8')))
