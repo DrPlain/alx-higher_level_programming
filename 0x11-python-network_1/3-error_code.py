6194f2d66e270ef68133b13db6796cf441cc8c19
Gideon Obiasor <gideonoiasor@gmail.com>
2023-01-17 20:52:29 +0100

    Task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Sends a request to an url and displays the body of response """
+from urllib import request, error
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode('UTF-8'))
+    except error.HTTPError as err:
+        print(f"Error: {err.code}")
