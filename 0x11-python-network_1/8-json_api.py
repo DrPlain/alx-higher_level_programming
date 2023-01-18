c702fbf7f9003484d9ac8c1d5d07e862d7db2924
Gideon Obiasor <gideonoiasor@gmail.com>
2023-01-18 05:35:16 +0100

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Takes in letter and sends a POST resquest to url given with
+the letter as parameter
+"""
+import requests
+fron sys import argv
+if __name__ == "__main__":
+    if argv[1]:
+        payload = {'q': argv[1]}
+    else:
+        payload = {'q': ""}
+    r = requests.post("http://0.0.0.0:5000/search_user", payload)
+    try:
+        res = r.json()
+        if res == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(res.get("id"), res.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
