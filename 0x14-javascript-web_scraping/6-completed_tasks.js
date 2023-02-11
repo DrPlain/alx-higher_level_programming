60079bc810513c36839a32a82874e1f8c8f86cc5
Gideon Obiasor <gideonoiasor@gmail.com>
2023-02-11 14:52:24 +0100

    Task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -9,16 +9,20 @@ request(URL, function (error, response, body) {
   } else if (response.statusCode === 200) {
     const tasks = JSON.parse(body);
     const dict = {};
-    const noUsers = tasks[tasks.length - 1].userId;
-    for (let id = 1; id <= noUsers; id++) {
-      let count = 0;
-      for (let i = 0; i < tasks.length; i++) {
-        if (tasks[i].completed === true && tasks[i].userId === id) {
-          count++;
+    if (tasks.length === 0) {
+      console.log(dict);
+    } else {
+      const noUsers = tasks[tasks.length - 1].userId;
+      for (let id = 1; id <= noUsers; id++) {
+        let count = 0;
+        for (let i = 0; i < tasks.length; i++) {
+          if (tasks[i].completed === true && tasks[i].userId === id) {
+            count++;
+          }
         }
+        dict[`${id}`] = count;
       }
-      dict[`${id}`] = count;
+      console.log(dict);
     }
-    console.log(dict);
   }
 });
